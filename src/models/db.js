160b8537e21a561587d05fef93766323586b0634
anukoolvikram <anukoolvikram777@gmail.com>
2025-05-19 16:42:37 +0530

    adding db

--- src/models/db.js
+++ src/models/db.js
@@ -3,14 +3,23 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT || 5432,
+//     max: 10, 
+//     ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+// });
+
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 5432,
-    max: 10, 
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false } // for Render
+    : false,
+  max: 10
 });
+
 
 module.exports = pool;
